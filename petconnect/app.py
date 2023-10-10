04b5ab6d2b9a00a4188fb1d1bdea0ae9f79a025e
gsandoo <rogersddt@naver.com>
2023-10-11 01:10:06 +0900

    Docs: app.py 수정

--- petconnect/app.py
+++ petconnect/app.py
@@ -45,4 +45,4 @@ app = create_app()
 if __name__ == "__main__":
     # app.config['TRAP_HTTP_EXCEPTIONS']=True
     # app.register_error_handler(Exception,serverErrorHandler)
-    app.run(host='0.0.0.0', port = 5000 , debug=True)
+    app.run(host='0.0.0.0', port = '5000' , debug=True)
