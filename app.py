a84144efd32008dd7ab3af705944511f424566ee
gsandoo <rogersddt@naver.com>
2023-10-27 00:45:03 +0900

    nose detection & disease refactor

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ db=SQLAlchemy()
 migrate=Migrate()
 
 app = Flask(__name__)
-    
 
 #flask 애플리케이션 팩토링 -> app 전역변수 지양
 def create_app():
+    app = Flask(__name__)
     
     # app.config.from_object(config)
 
@@ -29,12 +29,15 @@ def create_app():
     from views import nose_views
     app.register_blueprint(nose_views.bp)
     
-    # from views import disease_views
-    # app.register_blueprint(disease_views.bp)
+    from views import disease_views
+    app.register_blueprint(disease_views.bp)
+    
+    from views import eye_views
+    app.register_blueprint(eye_views.bp)
     
     @app.route('/' ,methods=['GET', 'POST'])
     def connect_with_android():
-        return "Flask Server & Cloud Functions are Working Successfully"
+        return "Flask Server & Android are Working Successfully"
     return app
 
 
@@ -46,4 +49,4 @@ app = create_app()
 if __name__ == "__main__":
     # app.config['TRAP_HTTP_EXCEPTIONS']=True
     # app.register_error_handler(Exception,serverErrorHandler)
-    app.run(host='0.0.0.0', port = '5000' , debug=True)
+    app.run(host='0.0.0.0', port = 8000 , debug=True)
